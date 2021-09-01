0346d82652413d7aa31bd85e3421ca0930362a4b
Wagner Inácio de Oliveira <wagner.inacio16@outlook.com>
2021-08-31 23:58:19 -0300

    Form da consulta

--- lib/views/appointment_form.dart
+++ lib/views/appointment_form.dart
@@ -1,7 +1,12 @@
 import 'package:agendamed/data/data_appointments.dart';
+import 'package:agendamed/models/appointment.dart';
 import 'package:agendamed/models/patient.dart';
+import 'package:agendamed/provider/appointments.dart';
+import 'package:agendamed/provider/doctors.dart';
+import 'package:agendamed/provider/patients.dart';
 import 'package:agendamed/views/appointment_list.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AppointmentForm extends StatefulWidget {
   const AppointmentForm({Key? key}) : super(key: key);
@@ -16,6 +21,7 @@ class _AppointmentFormState extends State<AppointmentForm> {
   TextEditingController _date = TextEditingController();
   TextEditingController _hour = TextEditingController();
   TextEditingController _insurance = TextEditingController();
+
   final _formKey = GlobalKey<FormState>();
 
   //late final Patient patient;
@@ -27,9 +33,26 @@ class _AppointmentFormState extends State<AppointmentForm> {
       date = "",
       hour = "",
       insurance = "";
+  final List<String> listItemPatient = [
+    'Walter Kanneman',
+    'Mathias Villasanti',
+    'Gabriel Chapecó',
+  ];
+  final List<String> listItemDoctor = [
+    'Douglas Costa',
+    'Miguel Borja',
+    'Pedro Geromel'
+  ];
+
+  late String valueChoosePatient = listItemPatient.first;
+  late String valueChooseDoctor = listItemDoctor.first;
 
   @override
   Widget build(BuildContext context) {
+    final Appointments appointments = Provider.of(context);
+    final Patients patients = Provider.of(context);
+    final Doctors doctors = Provider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Cadastrar Consulta"),
@@ -42,33 +65,55 @@ class _AppointmentFormState extends State<AppointmentForm> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                TextFormField(
-                  controller: _namePatient,
-                  keyboardType: TextInputType.name,
+                DropdownButtonFormField<String>(
                   decoration: InputDecoration(
                     labelText: "Nome do paciente:",
                     border: OutlineInputBorder(),
                     prefixIcon: Icon(Icons.person),
                   ),
-                  validator: (valor) {
-                    if (valor!.isNotEmpty) {
-                      return null;
-                    } else {
-                      return "Campo em branco";
-                    }
-                  },
+                  value: valueChoosePatient,
+                  items: listItemPatient
+                      .map(
+                        (item) => DropdownMenuItem<String>(
+                          child: Text(
+                            item,
+                          ),
+                          value: item,
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (value) => setState(() {
+                    this.valueChoosePatient = valueChoosePatient;
+                  }),
                 ),
-                // DropdownButtonFormField(
-
-                //   decoration: InputDecoration(
-                //     labelText: "Médico:",
-                //     border: OutlineInputBorder(),
-                //     //   prefixIcon: Icon(Icons.person),
-                //   ),
-                //   onChanged: (newValue) {
-                //     setState(() {});
-                //   }, items: [],
-                // ),
+                SizedBox(
+                  height: 20,
+                ),
+                DropdownButtonFormField<String>(
+                  decoration: InputDecoration(
+                    labelText: "Nome do Médico:",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                  value: valueChooseDoctor,
+                  items: listItemDoctor
+                      .map(
+                        (item) => DropdownMenuItem<String>(
+                          child: Text(
+                            item,
+                            //style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          value: item,
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (value) => setState(() {
+                    this.valueChooseDoctor = valueChooseDoctor;
+                  }),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
                 TextFormField(
                   controller: _date,
                   keyboardType: TextInputType.datetime,
@@ -84,6 +129,9 @@ class _AppointmentFormState extends State<AppointmentForm> {
                       return "Campo em branco";
                     }
                   },
+                ),
+                SizedBox(
+                  height: 20,
                 ),
                 TextFormField(
                   controller: _hour,
@@ -101,6 +149,9 @@ class _AppointmentFormState extends State<AppointmentForm> {
                     }
                   },
                 ),
+                SizedBox(
+                  height: 20,
+                ),
                 TextFormField(
                   controller: _insurance,
                   keyboardType: TextInputType.text,
@@ -117,33 +168,28 @@ class _AppointmentFormState extends State<AppointmentForm> {
                     }
                   },
                 ),
-                Container(
-                  width: 200,
-                  color: Colors.blue,
-                  child: TextButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        setState(() {});
-                        namePatient = _namePatient.text;
-                        nameDoctor = _nameDoctor.text;
-                        date = _date.text;
-                        hour = _hour.text;
-                        insurance = _insurance.text;
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (BuildContext context) =>
-                                    AppointmentList()));
-                      } else {}
-                    },
-                    child:
-                        Text("Agendar", style: TextStyle(color: Colors.white)),
-                  ),
+                SizedBox(
+                  height: 20,
                 ),
               ],
             ),
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          appointments.put(Appointment(
+              id: -1,
+              patient: patients.byIndex(0),
+              doctor: doctors.byIndex(0),
+              date: _date.text,
+              hour: _hour.text,
+              insurance: _insurance.text,
+              newAppointment: true));
+          Navigator.of(context).pop();
+        },
+        label: Text('Agendar'),
+        icon: Icon(Icons.person_add_alt_1_sharp),
       ),
     );
   }
